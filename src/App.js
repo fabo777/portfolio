4aa1721a493b0d45e5148db4f20acde942acb3cc
fabo777 <mario.fabijanec@gmail.com>
2022-05-02 16:29:10 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React, { useState } from "react";
-import BasicModal from "./Component/BasicModal";
-import { ContactUs } from "./Component/ContactUs";
 import myProjects from "./Data/myProjects.json";
-import Links from "./Component/Links";
 import useScreenWidth from "./Component/useScreenWidth";
-import MarioFabijanecCV from "./Mario Fabijanec CV.pdf";
+import LeftContainer from "./Component/LeftContainer";
+import Section1 from "./Component/Section1";
+import Section2 from "./Component/Section2";
+import Section3 from "./Component/Section3";
+import BasicModal from "./Component/BasicModal";
+import Links from "./Component/Links";
 
 const App = () => {
   const [open, setOpen] = React.useState(false);
@@ -23,81 +25,15 @@ const App = () => {
 
   return (
     <>
-      <div className="maincontainer">
+      <div className="mainContainer">
         <div className="row">
-          <div className="left">
-            <div className="leftContainer">
-              <div>
-                <img className="imageAvatar" src="images/avatar2.PNG" alt="" />
-                <h2>
-                  Hi, my name is Mario Fabijanec and I am a web developer from
-                  Zagreb. Welcome to my portfolio.
-                </h2>
-              </div>
-
-              {ScreenWidth > 850 && <Links />}
-            </div>
-          </div>
-
+          <LeftContainer />
           <div className="right">
-            <div className="section1">
-              <h1>Self-taught front-end developer</h1>
-              <div>
-                If you are looking for a new front-end junior developer, with a
-                high drive to learn new skills and improve existing ones, you
-                are in the right place. I have a solid knowledge in HTML, CSS,
-                JavaScript, React and Redux.
-              </div>
-              <a
-                href={MarioFabijanecCV}
-                download="Mario Fabijanec CV"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <button>Download CV</button>
-              </a>
-            </div>
-
+            <Section1 />
             <hr className="line"></hr>
-
-            <div className="section2">
-              <h2>Recent work</h2>
-              <div className="row2">
-                {myProjects.map((project) => {
-                  return (
-                    <div key={project.id} className="projects">
-                      <a href="#" className="image">
-                        <img
-                          onClick={() => {
-                            handleOpen(project.name);
-                            setGetID(project.id);
-                          }}
-                          className="img"
-                          src={`images/projects/${project.name}.PNG`}
-                          alt=""
-                        />
-                      </a>
-
-                      <h3>{project.name}</h3>
-                      <p>{project.msg}</p>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-
+            <Section2 handleOpen={handleOpen} setGetID={setGetID} />
             <hr className="line"></hr>
-
-            <div id="section3" className="section3">
-              <h2>Get In Touch</h2>
-              <div className="section3div">
-                If you are interested in giving me a chance feel free to contact
-                me and I will make sure to respond as soon as possible. In case
-                you dont like forms you can call me or send an email, needed
-                info can be find in my CV.
-              </div>
-              <ContactUs />
-            </div>
+            <Section3 />
           </div>
         </div>
       </div>
--- src/Component/Links.js
+++ src/Component/Links.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Links.css";
 
 const Links = () => {
   return (
